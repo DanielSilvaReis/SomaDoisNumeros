5e389c3ac1d366ce8d38a366013f3c5ec068e664
DanielSilvaReis <121450076+DanielSilvaReis@users.noreply.github.com>
2023-01-01 14:58:08 -0300

    Create Soma.py

---
+++ Soma.py
@@ -0,0 +1,9 @@
+""" Recebe as variáveis"
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+""" Operação de Soma"""
+s = n1 + n2
+
+"""Imprime os valores"""
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
